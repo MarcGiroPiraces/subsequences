c926e786442e02f6268a3c7a5e88cf5bcce45bcc
Marc Giro <marc.piraces@cognativinc.com>
2024-02-11 19:14:19 +0100

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import dotenv from 'dotenv';
 import express, { Application, Request, Response } from 'express';
-
-dotenv.config();
+import { PORT } from './config';
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Server');
