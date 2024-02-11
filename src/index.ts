aa6b004eda00e1f2dbb23093d3668faaaa5ac97c
Marc Giro <marc.piraces@cognativinc.com>
2024-02-11 21:00:09 +0100

    added token route

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express, { Application, Request, Response } from 'express';
+import { generateJwtToken } from './auth/generateJwtToken';
 import { PORT } from './config';
+import { auth } from './middleware/auth';
 
 const app: Application = express();
-const port = PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', auth, (req: Request, res: Response) => {
   res.send('Welcome to the Server');
 });
 
-app.listen(port, () => {
-  console.log(`Server is up at http://localhost:${port}`);
+app.get('/token', (req: Request, res: Response) => {
+  const token = generateJwtToken();
+  res.send(token);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is up at http://localhost:${PORT}`);
 });
