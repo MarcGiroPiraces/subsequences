0329672c5bf899a5d7d16c499513c217752850d2
Marc Giro <marc.piraces@cognativinc.com>
2024-02-11 19:59:50 +0100

    added secret key to config

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,4 @@ dotenv.config({
 
 export const PORT = process.env.PORT || 8000;
 export const CONNECTION_STRING = process.env.CONNECTION_STRING as string;
+export const SECRET_KEY = process.env.SECRET_KEY as string;
