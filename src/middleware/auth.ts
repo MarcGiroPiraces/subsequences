6aa66d910fb6bb9a0b7e1f8892c3c6f9a93bdf77
Marc Giro <marc.piraces@cognativinc.com>
2024-02-11 20:59:32 +0100

    refactored auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,11 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verifyJwtToken } from '../auth/verifyJwtToken';
 
 export const auth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header('auth-token');
-  if (!token) {
+  const authorizationHeader = req.header('Authorization');
+  if (!authorizationHeader) {
     return res.status(401).send('Access Denied');
   }
   try {
+    const token = authorizationHeader.split(' ')[1];
     const verified = verifyJwtToken(token);
     if (!verified) {
       return res.status(401).send('Invalid Token');
