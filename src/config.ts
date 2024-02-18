4ef08924bedee64a4b1ce366790651e12c0f2f8b
Marc Giro <marc.piraces@cognativinc.com>
2024-02-18 11:15:28 +0100

    updated env variables and database file

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,5 @@ dotenv.config({
 export const PORT = process.env.PORT || 8000;
 export const CONNECTION_STRING = process.env.CONNECTION_STRING as string;
 export const SECRET_KEY = process.env.SECRET_KEY as string;
+export const DATABASE_NAME = process.env.DATABASE_NAME as string;
+export const DATABASE_COLLECTION = process.env.DATABASE_COLLECTION as string;
